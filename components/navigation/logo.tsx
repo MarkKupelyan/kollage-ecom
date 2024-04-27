0df365a84f99842b54a1730122c9bd3ef1760187
W0rt3x <markdavokup@gmail.com>
2024-04-28 00:35:16 +0200

    initial signin page setup

---
+++ components/navigation/logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div>
+      <img src="/Kollage2-1.png" alt="Logo" width="212" height="89" />
+    </div>
+  );
+}
---
+++ server/migrations/0001_dapper_talisman.sql
@@ -0,0 +1,46 @@
+DO $$ BEGIN
+ CREATE TYPE "roles" AS ENUM('user', 'admin');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "account" (
+	"userId" text NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text,
+	CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "email_tokens" (
+	"id" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	"email" text NOT NULL,
+	CONSTRAINT "email_tokens_id_token_pk" PRIMARY KEY("id","token")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text NOT NULL,
+	"emailVerified" timestamp,
+	"image" text,
+	"password" text,
+	"twoFactorEnabled" boolean DEFAULT false,
+	"roles" "roles" DEFAULT 'user'
+);
+--> statement-breakpoint
+DROP TABLE "post";--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
